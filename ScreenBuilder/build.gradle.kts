527c834d8ff2416339e76b7b2d727747f266408c
rakeshchander <rakeshchander.cse@gmail.com>
2022-02-19 16:16:46 +0530

    Added Publish Support

--- ScreenBuilder/build.gradle.kts
+++ ScreenBuilder/build.gradle.kts
@@ -4,14 +4,22 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     kotlin("plugin.serialization")
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.dokka") version "1.6.10"
 }
+
+group = "io.github.rakeshchander"
+version = "1.0.0"
 
 kotlin {
 
     val serializationVersion = "1.3.2"
     val kotlinVersion = "1.6.10"
 
-    android()
+    android {
+        publishLibraryVariants("release")
+    }
     
     val xcf = XCFramework()
     listOf(
@@ -75,4 +83,96 @@ android {
         minSdk = 21
         targetSdk = 32
     }
+}
+
+val dokkaOutputDir = "$buildDir/dokka"
+/**
+ * This task generates documentation
+ */
+tasks.dokkaHtml {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+
+/**
+ * This task deletes older documents
+ */
+val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+    delete(dokkaOutputDir)
+}
+
+/**
+ * This task creates JAVA Docs for Release
+ */
+//val javadocJar = tasks.register<Jar>("javadocJar") {
+//    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+//    archiveClassifier.set("javadoc")
+//    from(dokkaOutputDir)
+//}
+
+// Empty javadoc
+val javadocJar = tasks.register("javadocJar", Jar::class.java) {
+    archiveClassifier.set("javadoc")
+}
+
+val sonatypeUsername: String? = System.getenv("SONATYPE_USERNAME")
+val sonatypePassword: String? = System.getenv("SONATYPE_PASSWORD")
+
+/**
+ * Publishing Task for MavenCentral
+ */
+publishing {
+    repositories {
+        maven {
+            name="kotlin"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+    }
+
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("kotlin")
+                description.set("<LIBRARY_DESCRIPTION>")
+                licenses {
+                    license {
+                        name.set("Apache")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
+                url.set("<GIT_LIBRARY_URL>")
+                issueManagement {
+                    system.set("Github")
+                    url.set("<GIT_LIBRARY_ISSUES_URL>")
+                }
+                scm {
+                    connection.set("<GIT_LIBRARY_CLONE_PATH>")
+                    url.set("<GIT_LIBRARY_URL>")
+                }
+                developers {
+                    developer {
+                        name.set("<NAME>")
+                        email.set("<EMAIL>")
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Signing JAR using GPG Keys
+ */
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("GPG_PRIVATE_KEY"),
+        System.getenv("GPG_PRIVATE_PASSWORD")
+    )
+    sign(publishing.publications)
 }
